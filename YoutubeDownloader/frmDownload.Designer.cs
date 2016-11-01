b5ea15f8833449d9c4d3608c07f3dcbf3071a437
Sreejath Sreeraman <sreejath@outlook.com>
2016-11-01 00:14:18 -0400

    Initial Commit. Main form and About box laid out. Code to access clipboard written

---
+++ YoutubeDownloader/frmDownload.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace YoutubeDownloader
+{
+    partial class frmDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(86, 33);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(759, 31);
+            this.txtURL.TabIndex = 0;
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(852, 30);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(144, 42);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "&Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.Location = new System.Drawing.Point(491, 616);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(155, 51);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmDownload
+            // 
+            this.AcceptButton = this.btnDownload;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClose;
+            this.ClientSize = new System.Drawing.Size(1380, 775);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.txtURL);
+            this.Name = "frmDownload";
+            this.Text = "Download";
+            this.Activated += new System.EventHandler(this.frmDownload_Activated);
+            this.Load += new System.EventHandler(this.frmDownload_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
---
+++ YoutubeDownloader/frmDownload.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeDownloader
+{
+    public partial class frmDownload : Form
+    {
+        public frmDownload()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(txtURL.Text, "Boom");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void frmDownload_Load(object sender, EventArgs e)
+        {
+            if (Clipboard.ContainsText())
+            {
+                txtURL.Text = Clipboard.GetText();
+            }
+
+        }
+
+        private void frmDownload_Activated(object sender, EventArgs e)
+        {
+            if ((txtURL.Text.Length == 0) && Clipboard.ContainsText())
+            {
+                txtURL.Text = Clipboard.GetText();
+            }
+        }
+    }
+}
